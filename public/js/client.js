d68df65e22a371dadd7971da7fdf308faf19f3df
syku <sykuningen@protonmail.ch>
2020-03-29 00:19:23 +1300

    Show online users count in navbar

--- public/js/client.js
+++ public/js/client.js
@@ -1,1 +1,9 @@
 const socket = io();
+
+$(() => {
+    socket.on('server_stats', (stats) => {
+        if (stats.online_users) {
+            $('#navbar-online-users').html(stats.online_users.toString() + ' user(s) online');
+        }
+    });
+});
--- server.py
+++ server.py
@@ -14,16 +14,31 @@ app = socketio.WSGIApp(sio, static_files=static_files)
 
 port = 8000
 
+# Server runtime data
+online_users = 0
+
 
 # *============================================================= SOCKET.IO
 @sio.on('connect')
 def connect(sid, env):
     print(f'Connection from SID {sid}')
 
+    global online_users
+    online_users += 1
+    sio.emit('server_stats', {
+        'online_users': online_users
+    })
+
 
 @sio.on('disconnect')
 def disconnect(sid):
     print(f'Disconnection from SID {sid}')
+
+    global online_users
+    online_users -= 1
+    sio.emit('server_stats', {
+        'online_users': online_users
+    })
 
 
 # *============================================================= MAIN
