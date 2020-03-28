a19cff6a19592484010a02a792072e56e4f7c293
syku <sykuningen@protonmail.ch>
2020-03-28 23:58:54 +1300

    Add Socket.IO

--- server.py
+++ server.py
@@ -5,13 +5,25 @@ import socketio
 # *============================================================= SERVER INIT
 static_files = {
     '/': 'pages/index.html',
-    '/css/default.css': 'public/css/default.css'
+    '/css/default.css': 'public/css/default.css',
+    '/js/client.js': 'public/js/client.js'
 }
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files=static_files)
 
 port = 8000
+
+
+# *============================================================= SOCKET.IO
+@sio.on('connect')
+def connect(sid, env):
+    print(f'Connection from SID {sid}')
+
+
+@sio.on('disconnect')
+def disconnect(sid):
+    print(f'Disconnection from SID {sid}')
 
 
 # *============================================================= MAIN
